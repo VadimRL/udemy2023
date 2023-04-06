be995faa4c5e2639f62e10a9b79bebbe0bcfe383
Vadim <vadicrugun@gmail.com>
2023-04-06 16:52:50 +0300

    24. Практика, ч2. Применяем условия и циклы

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-/* Задание на урок:
+/* 
+|------------------------------------|
+|------- 19. Задание на урок: -------|
+|------------------------------------|
 
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+1) Создать переменную movieRating и в неё поместить ответ от пользователя на вопрос:
 'Сколько фильмов вы уже посмотрели?'
 
 2) Создать объект personalMovieDB и в него поместить такие свойства:
@@ -19,23 +22,66 @@
         'logan': '8.1'
     }
 
-Проверить, чтобы все работало без ошибок в консоли */
+Проверить, чтобы все работало без ошибок в консоли 
 
-'use strict';
+|------------------------------------|
+|------- 24. Задание на урок: -------|
+|------------------------------------|
 
-const numberOfFilms = prompt("How many movies have you watched?", 0);
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+"use strict";
+
+let movieRating = 0;
+
+do {
+    movieRating = prompt("How many movies have you watched?", 0);
+    console.log(movieRating == null);
+} while (movieRating == null || movieRating.trim() == "" || isNaN(movieRating) || movieRating >= 50);
 
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: movieRating,
     movies: {},
     actors: {},
     genres: [],
     privat: false,
 };
 
-for(let i = 0; i < 2; i++) {
+// userNumber?.trim() == "" || !isFinite(userNumber) || (userNumber !== null && userNumber <= 100)
+for (let i = 0; i < 2; i++) {
+    console.log(i);
     const lastMovie = prompt("What is the name of the last movie you watched", "");
-    const movieRating = prompt("How would you rate it?","");
+    const movieRate = prompt("How would you rate it?", "");
 
-    personalMovieDB.movies[lastMovie] = movieRating;
+    if (
+        lastMovie == "" ||
+        movieRate == "" ||
+        isNaN(movieRate) ||
+        lastMovie == null ||
+        movieRate == null ||
+        lastMovie.length > 50 ||
+        movieRate > 50
+    ) {
+        i--;
+    } else {
+        personalMovieDB.movies[lastMovie] = movieRate;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    alert("Watched quite a few movies");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert("You are a classicial movie viewer");
+} else {
+    alert("You are a movie buff");
 }
